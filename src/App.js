ff82d88f216ab37e06eae8fb303be4484d16419b
javadAlip <javadalipp911@gmail.com>
2023-12-04 11:25:34 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect } from 'react'
+// e47508c1
+const API_URL='http://www.omdbapi.com?apikey=e47508c1'
+const App = () => {
+  const searchMovies=async(title)=>{
+    const response=await fetch(`${API_URL}&s=${title}`)
+    const data=await response.json()
+    console.log(data.Search)
 
-function App() {
+  }
+  useEffect(()=>{
+  searchMovies('Ironman')
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
